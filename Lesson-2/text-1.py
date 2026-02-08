d60602c5667415914e0de6c4a15e48186a8d8c49
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2026-02-08 21:33:04 +0000

    Add initial implementation of vocabulary building and bag-of-words vectorization functions

---
+++ Lesson-2/text-1.py
@@ -0,0 +1,20 @@
+def build_vocabs(docs):
+    unique_words = set()
+    for doc in docs:
+        for word in doc.lower().split():
+            clean_word = word.strip(".,!?")
+            if clean_word:
+                unique_words.add(clean_word)
+    return {word: idx for idx, word in enumerate(sorted(unique_words))}
+
+
+import numpy as np
+
+def bow_vectorize(text , vocab):
+    vector = np.zeros(len(vocab), dtype=int)
+    for word in text.lower().split():
+        clean_word = word.strip(".,!?")
+        if clean_word in vocab:
+            index = vocab[clean_word]
+            vector[index] += 1
+    return vector
